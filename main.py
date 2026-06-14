6f900b4b9ced84e3d3a200f53a2a903542dcf976
ssemtke25-png <ssemtke25@gmail.com>
2026-06-14 12:05:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -201,6 +201,7 @@ if 'active_tab' not in st.session_state:
 if 'saved_region' not in st.session_state:
     st.session_state.saved_region = "포항시"
     if 'unlocked_region' not in st.session_state:
+    
     st.session_state.unlocked_region = None
 
 def render_safe_html(text, kw=""):
