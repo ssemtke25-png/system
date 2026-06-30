cd047dd9a4fbfdf6ccfa93b58b6f112cb4822574
ssemtke25-png <ssemtke25@gmail.com>
2026-06-30 19:28:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
 import os
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "excel_system"))
-
-from modules import tab7_hwpx_ai
 import json
 import re
 import base64
